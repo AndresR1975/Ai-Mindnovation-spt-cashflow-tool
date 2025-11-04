d10fda5435bb6690eed36c93f617a5d9f137c332
Andres Restrepo Ayala <84421369+AndresR1975@users.noreply.github.com>
2025-11-04 09:18:39 -0500

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -200,7 +200,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-from datetime import datetime
+from datetime import datetime, timedelta
 import numpy as np
 from io import BytesIO
 
